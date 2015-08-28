e795bfaef2144e7630ce6960abee8b37935def0f
vincent <wenxin.he@g>
2015-08-28 22:32:36 +0800

    adjust view and security

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,6 @@
 class CoursesController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  http_basic_authenticate_with name: "dhh", password: "secret"
   
   def new
     @course = Course.new
